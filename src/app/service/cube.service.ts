b9740c1550962ffd01111b20ae30e66fce52f565
xmltjl <xmltjl@gmail.com>
2020-08-14 16:10:57 +1000

    clean up

--- src/app/service/cube.service.ts
+++ src/app/service/cube.service.ts
@@ -5,12 +5,8 @@ import { Cubes } from '../model/tm1';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    // generated via postman
-    //Authorization: 'Basic QWRtaW46YXBwbGU=',
-    //Authorization: 'Basic base64(mel:melpwd)',
     'Content-Type': 'application/json',
     Authorization: 'Basic YWRtaW46',
-    //Cookie: 'TM1SessionId=gsR4rNFmn5DM7NyFXmu7rg',
   }),
 };
 
@@ -18,20 +14,17 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class CubeService {
-  //https://medium.com/@lucian.cbn/catcomponent-html-3-5cb2e33a07d
   url: string = 'https://localhost:44312/api/v1';
 
   constructor(private http: HttpClient) {}
 
   getData1(cubeName: string, viewName: string): Observable<Cubes> {
-    //https://stackoverflow.com/questions/47757655/how-to-add-headers-to-my-angular-post-request/47757775
     const href = `${this.url}/Cubes('${cubeName}')/Views('${viewName}')/tm1.Execute?$expand=Cells`;
     return this.http.post<Cubes>(href, null, httpOptions);
   }
 
   // hard code mdx to test
   getData(cubeName: string, viewName: string): Observable<Cubes> {
-    //https://stackoverflow.com/questions/36689594/getting-data-from-cognos-tm1-via-rest-api
     const mdx = `{
       "MDX": "SELECT {[BT Calendar Period].[202001], [BT Calendar Period].[202002]} ON COLUMNS, {[HS Centre].[MI View]} ON ROWS FROM [HS Summary]"
     }`;
